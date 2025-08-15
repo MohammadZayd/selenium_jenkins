705b1dac4116d751d1afdca42404b0645f745d6b
mohammadzayd <106404449+MohammadZayd@users.noreply.github.com>
2025-08-15 14:33:46 +0300

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -3,21 +3,19 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-# Set up Chrome options for Jenkins/Docker environment
+# Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920,1080")  # Set viewport size
 
 # Initialize Chrome WebDriver
 driver = webdriver.Chrome(options=chrome_options)
 
 # Open a webpage
-driver.get("https://aws.amazon.com/")
+driver.get("https://www.amazon.sa")
 time.sleep(5)  # Wait for the page to load
-
-# Make the browser full screen
-driver.fullscreen_window()
 
 # Take screenshot
 screenshot_path = os.path.join(os.getcwd(), "aws_screenshot.png")
